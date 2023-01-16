d875428dddba563c4b17ef2b566c1b0c325deeb1
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-01-17 05:45:25 +0600

    solved linters issues

--- index.js
+++ index.js
@@ -1,92 +1,89 @@
-import BookList from "../modules/BookList.js";
-import Book from "./modules/Book.js";
-import { DateTime } from "./luxon.js";
+import BookList from './modules/BookList.js';
+import { DateTime } from './luxon.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const booksLink = document.querySelector('.list');
-    const formsLink = document.querySelector('.add-new');
-    const contactsLink = document.querySelector('.contact-link');
-    const booksPage = document.querySelector('.book-list');
-    const formsPage = document.querySelector('.form');
-    const contactsPage = document.querySelector('.contact');
-    const booksTitle = document.querySelector('.books-head');
+  const booksLink = document.querySelector('.list');
+  const formsLink = document.querySelector('.add-new');
+  const contactsLink = document.querySelector('.contact-link');
+  const booksPage = document.querySelector('.book-list');
+  const formsPage = document.querySelector('.form');
+  const contactsPage = document.querySelector('.contact');
+  const booksTitle = document.querySelector('.books-head');
+  formsPage.classList.add('hide');
+  contactsPage.classList.add('hide');
+
+  const showListOnly = () => {
     formsPage.classList.add('hide');
     contactsPage.classList.add('hide');
-    
-    const showListOnly=()=> {
-      formsPage.classList.add('hide');
-      contactsPage.classList.add('hide');
-      booksPage.classList.remove('hide');
-      booksTitle.classList.remove('hide');
-    }
-    booksLink.addEventListener('click', showListOnly);
-    function showContactOnly() {
-      booksPage.classList.add('hide');
-      formsPage.classList.add('hide');
-      booksTitle.classList.add('hide');
-      contactsPage.classList.remove('hide');
-    }
-    contactsLink.addEventListener('click', showContactOnly);
-    
-    const showFormOnly=() => {
-      booksPage.classList.add('hide');
-      contactsPage.classList.add('hide');
-      booksTitle.classList.add('hide');
-      formsPage.classList.remove('hide');
-    }
-    formsLink.addEventListener('click', showFormOnly);
-  });
-  const date =  DateTime.local().toLocaleString(DateTime.DATETIME_FULL);
-  document.getElementById('date').innerHTML = date;
-  const Books = new BookList(JSON.parse(localStorage.getItem('Books')).Books || []);
-  const bookList = document.createElement('section');
-  bookList.className = 'book-list';
-  bookList.classList.add('show');
-  let bookIndex = 0;
-  
-  Books.Books.forEach((Book) => {
-    let bookClass = 'bookContainer';
-    if (bookIndex % 2 === 1) {
-      bookClass = 'bookContainer1';
-    }
-    const book = `
+    booksPage.classList.remove('hide');
+    booksTitle.classList.remove('hide');
+  };
+  booksLink.addEventListener('click', showListOnly);
+  function showContactOnly() {
+    booksPage.classList.add('hide');
+    formsPage.classList.add('hide');
+    booksTitle.classList.add('hide');
+    contactsPage.classList.remove('hide');
+  }
+  contactsLink.addEventListener('click', showContactOnly);
+
+  const showFormOnly = () => {
+    booksPage.classList.add('hide');
+    contactsPage.classList.add('hide');
+    booksTitle.classList.add('hide');
+    formsPage.classList.remove('hide');
+  };
+  formsLink.addEventListener('click', showFormOnly);
+});
+const date = DateTime.local().toLocaleString(DateTime.DATETIME_FULL);
+document.getElementById('date').innerHTML = date;
+const Books = new BookList(JSON.parse(localStorage.getItem('Books')).Books || []);
+const bookList = document.createElement('section');
+bookList.className = 'book-list';
+bookList.classList.add('show');
+let bookIndex = 0;
+
+Books.Books.forEach((Book) => {
+  let bookClass = 'bookContainer';
+  if (bookIndex % 2 === 1) {
+    bookClass = 'bookContainer1';
+  }
+  const book = `
       <div class="${bookClass}" id="${Book.id}">
           <p>
           ${Book.title} by ${Book.author}
           </p>
           <button class="remove">Remove</button>
       </div>`;
-    bookList.innerHTML += book;
-    bookIndex += 1;
-  });
-  
-  document.body.insertBefore(bookList, document.body.children[3]);
-  
-  const addButton = document.querySelector('#add-book');
-  const removeButton = document.querySelectorAll('.remove');
-  
-    const addBook = () => {
-      const formTitle = document.querySelector('#title');
-  
-      const formAuthor = document.querySelector('#author');
+  bookList.innerHTML += book;
+  bookIndex += 1;
+});
 
-      if (formAuthor.value && formTitle.value) {
-        Books.addBook(formTitle.value, formAuthor.value);
-        localStorage.setItem('Books', JSON.stringify(Books));
-        formAuthor.value = '';
-        formTitle.value = '';
-        window.location.reload();
-      }
-    }
-  
-  const  removeBook = (e) => {
-      const bookId = e.target.parentNode.id;
-      Books.removeBook(bookId)
-      localStorage.setItem('Books', JSON.stringify(Books));
-       window.location.reload()
-        }
-    
-  
-  addButton.addEventListener('click', addBook);
-  removeButton.forEach((element) => element.addEventListener('click', removeBook));
-  
+document.body.insertBefore(bookList, document.body.children[3]);
+
+const addButton = document.querySelector('#add-book');
+const removeButton = document.querySelectorAll('.remove');
+
+const addBook = () => {
+  const formTitle = document.querySelector('#title');
+
+  const formAuthor = document.querySelector('#author');
+
+  if (formAuthor.value && formTitle.value) {
+    Books.addBook(formTitle.value, formAuthor.value);
+    localStorage.setItem('Books', JSON.stringify(Books));
+    formAuthor.value = '';
+    formTitle.value = '';
+    window.location.reload();
+  }
+};
+
+const removeBook = (e) => {
+  const bookId = e.target.parentNode.id;
+  Books.removeBook(bookId);
+  localStorage.setItem('Books', JSON.stringify(Books));
+  window.location.reload();
+};
+
+addButton.addEventListener('click', addBook);
+removeButton.forEach((element) => element.addEventListener('click', removeBook));
--- modules/Book.js
+++ modules/Book.js
@@ -1,11 +1,9 @@
-import v4  from "../node_modules/uuid/dist/esm-browser/v4.js";
+import v4 from '../node_modules/uuid/dist/esm-browser/v4.js';
 
 export default class Book {
-    constructor(title,author){
-        this.id = v4();
-        this.title = title;
-        this.author = author;
-    }
-
-    
+  constructor(title, author) {
+    this.id = v4();
+    this.title = title;
+    this.author = author;
+  }
 }
